7c30006f887ebc904640d11ebb6a003935524480
abeaar <140606965+abeaar@users.noreply.github.com>
2024-03-05 20:03:22 +0700

    Update Mage.java

--- jrpg/src/jrpg/Mage.java
+++ jrpg/src/jrpg/Mage.java
@@ -5,19 +5,34 @@ package jrpg;
 public class Mage extends Newbie {
     private String weapon;
     private String skill;
+    private int mp;
+    private int strength;
+    private int dex;
 
     public Mage(String nama) {
-        super(nama, "Sorcerer");
+        super(nama);
+        setJob("Sorcerer");
         this.weapon = "Enchanted Cane";
         this.skill = "Lightning";
+        allocateStats(13, 5, 2);
+    }
+    
+    public void allocateStats(int mp, int strength, int dex) {
+        this.mp = mp;
+        this.strength = strength;
+        this.dex = dex;       
     }
 
     public void display(){
         super.display();
         System.out.println("weapon\t\t: " + this.weapon);
+        System.out.println("Stats:");
+        System.out.println("MP\t\t: " + this.mp);
+        System.out.println("Strength\t: " + this.strength);
+        System.out.println("Dexterity\t: " + this.dex);
     }
 
     public void useskill(){
-        System.out.println("Using Skill \t: " + this.skill);
+        System.out.println("Menggunakan\t: " + this.skill);
     }
 }
