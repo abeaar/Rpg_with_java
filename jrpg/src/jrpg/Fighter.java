2f199e4abad42515bf6cda8ad4e7b92710381e65
abeaar <140606965+abeaar@users.noreply.github.com>
2024-03-05 20:14:22 +0700

    Update Fighter.java

--- jrpg/src/jrpg/Fighter.java
+++ jrpg/src/jrpg/Fighter.java
@@ -5,20 +5,35 @@ package jrpg;
 public class Fighter extends Newbie {
     private String weapon;
     private String skill;
+    private int mp;
+    private int strength;
+    private int dex;
 
     public Fighter(String nama) {
-        super(nama, "Brawler");
-        this.weapon = "Knuckle Brass";
-        this.skill = "Jab"; 
+        super(nama);
+        setJob("Brawler");
+        this.weapon = "Knuckles Brass";
+        this.skill = "Jab";
+        allocateStats(2, 15, 3);
+    }
+    
+    public void allocateStats(int mp, int strength, int dex) {
+        this.mp = mp;
+        this.strength = strength;
+        this.dex = dex;       
     }
 
     public void display(){
         super.display();
         System.out.println("weapon\t\t: " + this.weapon);
+        System.out.println("Stats:");
+        System.out.println("MP\t\t: " + this.mp);
+        System.out.println("Strength\t: " + this.strength);
+        System.out.println("Dexterity\t: " + this.dex);
     }
+
 
     public void useskill(){
-        System.out.println("Using Skill \t: " + this.skill);
+        System.out.println("Menggunakan\t: " + this.skill);
     }
-
 }
